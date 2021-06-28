71a8a9fedf729f7b5ca27a14e52e3aa4a7eead2c
Kadyr-botokaraev <78466266+Kadyr-botokaraev@users.noreply.github.com>
2021-06-29 00:42:34 +0300

    Add files via upload

---
+++ EchoServer/mytcpserver.h
@@ -0,0 +1,39 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+#include <QtNetwork>
+#include <QByteArray>
+#include <QDebug>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+
+
+    void slotServerRead();
+    //QString message;
+    //QString shifr(message);
+    //void slotServerWrite();
+private:
+    QTcpServer * mTcpServer;
+    QTcpSocket * mTcpSocket;
+    int server_status;
+
+};
+#endif // MYTCPSERVER_H
+
+
+
+
+
+
+
